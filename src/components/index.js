e4e031419e820ccd5ba4689d945d4fba8e6bfc16
Md Rezaul Hossain <str.rezaul@gmail.com>
2024-12-08 18:46:15 +0600

    implement service-store-auth

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Input from "./Input";
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo, 
+    LogoutBtn,
+    Button,
+    PostCard,
+    Select,
+    Input,
+    
+}
